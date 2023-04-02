af9903d8f1341d2fdd1939c7e2b6c94e0c6f1bb6
Tomislav Vrbošić <tomislav.vrbosic@gmail.com>
2023-04-02 12:56:39 +0200

    Mock server /api/v1/survey endpoint

--- api/index.js
+++ api/index.js
@@ -1,11 +1,39 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-app.get('/api/v1/', (req, res) => {
-  res.json({ message: 'Hello from Express server!' });
+const internalServerErrorResponse = (res) => {
+  res.status(500).json({
+    errors: [
+      {
+        title: 'Internal Server Error',
+        detail: "Something went wrong. We're working on it!",
+      },
+    ],
+  });
+};
+
+app.get('/api/v1/survey', (req, res) => {
+  const file = path.join(process.cwd(), 'data', 'survey.json');
+
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      // Error reading file
+      internalServerErrorResponse(res);
+      return;
+    }
+
+    try {
+      const jsonData = JSON.parse(data);
+      return res.json(jsonData);
+    } catch (error) {
+      // Error parsing JSON
+      internalServerErrorResponse(res);
+    }
+  });
 });
 
 app.listen(PORT, () => {
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { useState, useEffect } from 'react';
-
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch('/api/v1/')
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
     <div>
       <div>Hello from React application!</div>
-      <div>{!data ? 'Loading ...' : data}</div>
     </div>
   );
 }
